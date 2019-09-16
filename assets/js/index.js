c6865e1bbb9bc7178ad34fcb1313fed8bf8f75c0
Gac125 <giulia.castro12@gmail.com>
2019-09-16 20:26:45 -0300

    apaguei

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  let db = coDesConnect('https://avengers-2.firebaseio.com/')
-
-  db.download('/', function(data) {
-  	console.log(data)
-    coDesReplace('.section-menu', data)
-  })
-})
